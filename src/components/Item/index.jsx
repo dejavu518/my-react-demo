07cdfe1e670f8469fa383ebca9f77a9309997436
dejavu518 <cf_1118sab@163.com>
2022-05-19 15:28:49 +0800

    fix: 添加鼠标移入移出事件

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import './index.css'
 export default class index extends Component {
+  state = {
+    mouse: false
+  }
+  // 鼠标移入移出事件
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({ mouse: flag })
+    }
+  }
   render() {
     const { name, done } = this.props
+    const { mouse } = this.state
     return (
-      <li>
+      <li onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)} style={{ background: mouse ? '#ddd' : 'white' }}>
         <label>
           <input type="checkbox" defaultChecked={done} />
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{ display: 'none' }}>删除</button>
-      </li>
+        <button className="btn btn-danger" style={{ display: mouse ? 'block' : 'none' }}>删除</button>
+      </li >
     )
   }
 }
