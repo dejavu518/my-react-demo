5de256813e3ef94b34e9ae2f89308f75c8665589
dejavu518 <cf_1118sab@163.com>
2022-05-25 15:42:22 +0800

    feat: github小案例

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,38 +2,23 @@ import React, { Component } from 'react'
 import './index.css'
 export default class List extends Component {
   render() {
+    const { users, isFirst, isLoading, err } = this.props
     return (
       <div className="row">
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="user-img" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="user-img" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="user-img" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="user-img" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
-        <div className="card">
-          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{ width: '100px' }} alt="user-img" />
-          </a>
-          <p className="card-text">reactjs</p>
-        </div>
+        {
+          isFirst ? <h2>欢迎使用</h2> :
+            isLoading ? <h2>Loading...</h2> :
+              users.map(userObj => {
+                return (
+                  <div className="card" key={userObj.id}>
+                    <a href={userObj.html_url} target="_blank" rel="noreferrer">
+                      <img src={userObj.avatar_url} style={{ width: '100px' }} alt="user-img" />
+                    </a>
+                    <p className="card-text">{userObj.login}</p>
+                  </div>
+                )
+              })
+        }
       </div>
     )
   }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export default class Search extends Component {
+  search = () => {
+    // 获取用户输入
+    const { value } = this.keyElement
+    // 发送请求前通知App更新状态
+    this.props.updateAppState({ isFirst: false, isLoading: true })
+    // 发送请求
+    axios.get(`https://api.github.com/search/users?q=${value}`).then(
+      res => {
+        this.props.updateAppState({ isLoading: false, users: res.data.items })
+      },
+      error => {
+        this.props.updateAppState({ isLoading: false, err: error.message })
+      }
+    )
+  }
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">Search Github Users</h3>
         <div>
-          <input type="text" placeholder="enter the name you search" />&nbsp;<button>Search</button>
+          <input type="text" placeholder="enter the name you search" ref={c => this.keyElement = c} />&nbsp;<button onClick={this.search}>Search</button>
         </div>
       </section>
     )
