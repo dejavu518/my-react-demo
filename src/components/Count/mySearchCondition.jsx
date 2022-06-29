81d4fc0dcc384de3653f49da0ca35f368d5a9b84
dejavu518 <cf_1118sab@163.com>
2022-06-29 15:56:08 +0800

    ...

--- src/components/Count/mySearchCondition.jsx
+++ src/components/Count/mySearchCondition.jsx
@@ -1,7 +1,6 @@
-import { Button, DatePicker, Input, message, Select, Space } from 'antd';
-import React, { useState } from 'react';
-import { getIntl, useModel } from 'umi';
-import styles from './MySearchCondition.less';
+import { Button, DatePicker, Form, Input, message, Select } from 'antd';
+import { useEffect, useState } from 'react';
+import { getIntl, useIntl, useModel } from 'umi';
 
 const { Option } = Select;
 const { RangePicker } = DatePicker;
@@ -37,236 +36,181 @@ const ConditionNode = conditionItems.map((item) => {
     </Option>
   );
 });
-
 const MySearchCondition = (props) => {
+  const intl = useIntl();
+  const [form1] = Form.useForm();
   const { search, queryTypes, rangePickerProps } = props;
   const queryLength = queryTypes.length;
-  const [searchRow, setSearchRow] = useState(1);
+  const [searchRow, setSearchRow] = useState(0);
+  const [keyRow, setKeyRow] = useState();
   const [addedRows, setAddedRows] = useState([]);
   const ObjectNode = queryTypes.map((item) => {
-    const obj = {
-      field_name: item.field_name || item.dataIndex,
-      name: item.name || item.title,
-      field_type: item.valueType,
-    };
-    return (
-      <Option key={obj.field_name} value={obj.field_name}>
-        {obj.name}
-      </Option>
-    );
-  });
-
-  const { LAYOUTMARGIN, LAYOUTPADDING } = useModel('Constant');
-  const getSearchNode = (key) => {
-    return (
-      <Space key={key} style={{ padding: '0 12px', marginBottom: '24px' }}>
-        <Select
-          value={fieldValue}
-          labelInValue
-          style={{
-            width: 120,
-          }}
-          onChange={(e) => {
-            handleFieldChange(e, key);
-          }}
-        >
-          {ObjectNode}
-        </Select>
-        <Select
-          labelInValue
-          value={conditionValue}
-          style={{
-            width: 120,
-          }}
-          onChange={(e) => {
-            handleConditionChange(e, key);
-          }}
-        >
-          {ConditionNode}
-        </Select>
-        {conditionAry[key - 1]?.value == BETWEEN ? (
-          <RangePicker
-            {...rangePickerProps}
-            onChange={(dates, dateStrs) => {
-              handleRangePickerChange(dateStrs, key);
-            }}
-          />
-        ) : (
-          <Input
-            value={queryValue}
-            onInput={(e) => {
-              handleInputChange(e, key);
-            }}
-          />
-        )}
-
-        {key == 1 ? (
-          <Button onClick={onAddSearchRow} type="">
-            +{/* <PlusSquareOutlined /> */}
-          </Button>
-        ) : (
-          ''
-        )}
-      </Space>
-    );
-  };
-  const onAddSearchRow = (e) => {
-    if (searchRow > queryLength - 1) {
-      message.warning(`Up to 5 ${queryLength} supported.`);
-      return;
-    } else {
-      setSearchRow(() => {
-        return searchRow + 1;
-      });
-      setAddedRows(() => {
-        const rowAry = [];
-        for (let i = 1; i < searchRow + 1; i++) {
-          rowAry.push(getSearchNode(i + 1));
-        }
-        return rowAry;
-      });
-    }
-  };
-
-  const [fieldValue, setFieldValue] = useState();
-  const [conditionValue, setConditionValue] = useState();
-  const [queryValue, setQueryValue] = useState();
-  const onReset = (e) => {
-    setSearchRow(() => {
-      return 1;
-    });
-    setFieldAry([]);
-    setConditionAry([]);
-    setValueAry([]);
-    setAddedRows(() => {
-      return [];
-    });
-    setFieldValue(null);
-    setConditionValue(null);
-    setQueryValue(null);
-  };
-  // let buttonLabel = '展开';
-  // const [explanded, setExplanded] = useState(false);
-  // const onToggle = (e) => {
-  //   if (explanded) {
-  //     buttonLabel = '收起';
-  //   } else {
-  //     buttonLabel = '展开';
-  //   }
-  //   setExplanded(() => {
-  //     return !explanded;
-  //   });
-  // };
-
-  const [fieldAry, setFieldAry] = useState([]);
-  const handleFieldChange = (e, key) => {
-    const keysAry = fieldAry.map((item) => {
-      if (item) {
-        return item.value;
-      }
-    });
-    if (keysAry.includes(e.value)) {
-      message.warning('There is same condition field already.');
-      return;
-    }
-    setFieldAry((data) => {
-      const copy = [...data];
-      copy[key - 1] = e;
-      return copy;
-    });
-    setFieldValue(() => {
-      return e.value;
-    });
-  };
-
-  const [conditionAry, setConditionAry] = useState([]);
-  const handleConditionChange = (e, key) => {
-    setConditionAry((data) => {
-      const copy = [...data];
-      copy[key - 1] = e;
-      return copy;
-    });
-    setConditionValue(() => {
-      return e.value;
-    });
-  };
-
-  const [valueAry, setValueAry] = useState([]);
-  const handleInputChange = (e, key) => {
-    console.log('input change');
-    setValueAry((data) => {
-      const copy = [...data];
-      copy[key - 1] = { value: e.target.value };
-      return copy;
-    });
-    setQueryValue(() => {
-      return e.target.value;
-    });
-  };
-  const handleRangePickerChange = (dateStrs, key) => {
-    const rangeDateValue = dateStrs.join('|');
-    setValueAry((data) => {
-      const copy = [...data];
-      copy[key - 1] = { value: rangeDateValue };
-      return copy;
-    });
-  };
-
-  const onSearch = () => {
-    let maxLength = Math.max(...[fieldAry.length, conditionAry.length, valueAry.length]);
-    const queryAry = [];
-    for (let i = 0; i < maxLength; i++) {
-      fieldAry[i] = fieldAry[i] || {};
-      conditionAry[i] = conditionAry[i] || {};
-      valueAry[i] = valueAry[i] || {};
-
-      queryAry[i] = {};
-      queryAry[i].queryname = fieldAry[i].value;
-      queryAry[i].queryoperator = conditionAry[i].value;
-      queryAry[i].queryvalue = valueAry[i].value;
-    }
-    const filteredQueryAry = queryAry.filter((item) => {
+    if (item.valueType !== 'index' && item.valueType !== 'option') {
+      const obj = {
+        field_name: item.field_name || item.dataIndex,
+        name: item.name || item.title,
+        field_type: item.valueType,
+      };
       return (
-        item.queryname !== undefined &&
-        item.queryoperator !== undefined &&
-        item.queryvalue !== undefined
+        <Option key={obj.field_name} value={obj.field_name}>
+          {obj.name}
+        </Option>
+      );
+    }
+  });
+  const onFinish = (values) => {
+    let arr = [];
+    for (let i in values) {
+      arr.push(values[i]);
+    }
+    arr = arr.filter((item) => {
+      return (
+        item &&
+        item.queryname &&
+        item.queryname !== '' &&
+        item.queryoperator &&
+        item.queryoperator !== '' &&
+        item.queryvalue &&
+        item.queryvalue !== ''
       );
     });
-    search(filteredQueryAry);
+    search(arr);
   };
-
+  const { LAYOUTMARGIN, LAYOUTPADDING } = useModel('Constant');
+  const getCountNode = (key) => {
+    return (
+      <div key={key} name={key}>
+        <Form.Item name={key} label="查询" style={{ display: 'inline-flex', marginBottom: '0px' }}>
+          <Input.Group>
+            <Form.Item
+              name={[key, 'queryname']}
+              style={{ display: 'inline-block', width: '150px' }}
+            >
+              <Select>{ObjectNode}</Select>
+            </Form.Item>
+            <Form.Item
+              name={[key, 'queryoperator']}
+              style={{ display: 'inline-block', width: '100px', margin: '0 8px' }}
+            >
+              <Select>{ConditionNode}</Select>
+            </Form.Item>
+            <Form.Item
+              name={[key, 'queryvalue']}
+              style={{ display: 'inline-block', width: '200px' }}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item style={{ display: 'inline-block', width: '10px', margin: '0 8px' }}>
+              <div
+                onClick={() => {
+                  setKeyRow(() => {
+                    return key;
+                  });
+                  setSearchRow(() => {
+                    return searchRow - 1;
+                  });
+                }}
+              >
+                -
+              </div>
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+      </div>
+    );
+  };
+  useEffect(() => {
+    setAddedRows(() => {
+      if (addedRows[keyRow]) {
+        let rowAry = [...addedRows];
+        rowAry.splice(keyRow, 1);
+        return rowAry;
+      } else {
+        let rowAry = [];
+        for (let i = 0; i < searchRow; i++) {
+          rowAry.push(getCountNode(i));
+        }
+        return rowAry;
+      }
+    });
+  }, [searchRow]);
   return (
-    <div
-      className={styles.MySearchCondition}
-      style={{
-        margin: `${LAYOUTMARGIN} -8px -8px`,
-        padding: `${LAYOUTPADDING} ${LAYOUTPADDING} 0`,
-      }}
-    >
-      <div>
-        {Intl.formatMessage({
-          id: 'component.myTable.search',
-          defaultMessage: '查询',
+    <div style={{ margin: '16px -8px -8px', padding: '16px 16px 0px', backgroundColor: '#fff' }}>
+      <Form
+        form={form1}
+        name="search"
+        initialValues={{ remember: true }}
+        autoComplete="off"
+        onFinish={onFinish}
+        preserve={false}
+      >
+        <Form.Item name="tem" label="查询" style={{ display: 'inline-flex', marginBottom: '0px' }}>
+          <Input.Group compact>
+            <Form.Item
+              name={['tem', 'queryname']}
+              style={{ display: 'inline-block', width: '150px' }}
+            >
+              <Select>{ObjectNode}</Select>
+            </Form.Item>
+            <Form.Item
+              name={['tem', 'queryoperator']}
+              style={{ display: 'inline-block', width: '100px', margin: '0 8px' }}
+            >
+              <Select>{ConditionNode}</Select>
+            </Form.Item>
+            <Form.Item
+              name={['tem', 'queryvalue']}
+              style={{ display: 'inline-block', width: '200px' }}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item style={{ display: 'inline-block', width: '10px', margin: '0 8px' }}>
+              <div
+                onClick={() => {
+                  const arr = ObjectNode.filter((item) => item !== undefined);
+                  if (searchRow > arr.length - 1 || searchRow == arr.length - 1) {
+                    const defaultLoginSuccessMessage = intl.formatMessage({
+                      id: 'pages.searchTable.max',
+                      defaultMessage: '达到最大数值无法添加！',
+                    });
+                    message.success(defaultLoginSuccessMessage);
+                  } else {
+                    setKeyRow(() => {
+                      return '';
+                    });
+                    setSearchRow(() => {
+                      return searchRow + 1;
+                    });
+                  }
+                }}
+              >
+                +
+              </div>
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+        <Form.Item style={{ display: 'inline-block', width: '64px', margin: '0 8px' }}>
+          <Button
+            type="primary"
+            onClick={() => {
+              form1.resetFields();
+              setSearchRow(() => {
+                return 0;
+              });
+            }}
+          >
+            重置
+          </Button>
+        </Form.Item>
+        <Form.Item style={{ display: 'inline-block', width: '64px', margin: '0 8px' }}>
+          <Button type="primary" htmlType="submit">
+            查询
+          </Button>
+        </Form.Item>
+        {addedRows.map((item) => {
+          return item;
         })}
-        <span>{getSearchNode(1)}</span>
-        <Space>
-          <Space>
-            <Button onClick={onReset} type="">
-              重置
-            </Button>
-            <Button onClick={onSearch} type="primary">
-              查询
-            </Button>
-          </Space>
-
-          {/* <Button onClick={onToggle} type="text">
-          {buttonLabel}
-        </Button> */}
-        </Space>
-      </div>
-      <div>
-        <span>{addedRows}</span>
-      </div>
+      </Form>
     </div>
   );
 };
